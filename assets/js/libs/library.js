a9b3faa31be1aa943f820f7d88f090348c0d1152
Kyle Adams <kadams54@users.noreply.github.com>
2021-04-21 14:32:27 -0400

    Style for contact page

--- assets/js/libs/library.js
+++ assets/js/libs/library.js
@@ -1,1 +1,5 @@
-console.log('Library')
+function encode(data) {
+  return Object.keys(data)
+    .map(key => encodeURIComponent(key) + "=" + encodeURIComponent(data[key]))
+    .join("&")
+}
---
+++ assets/js/pages/contact.js
@@ -0,0 +1,39 @@
+const LEVELS = {
+  SUCCESS: 'success',
+  DANGER: 'danger'
+};
+
+function feedback(message, level) {
+  const container = document.querySelector('#feedback');
+  for (const l in Object.values(LEVELS)) {
+    container.classList.remove(`text-${l}`);
+  }
+  container.classList.add(`text-${level}`);
+  container.textContent = message;
+}
+
+function handleSubmit(event) {
+  event.preventDefault();
+  const form = event.target;
+  fetch('/', {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+    body: encode({
+      'form-name': form.getAttribute('name'),
+      ...new FormData(form)
+    })
+  }).then(resp => {
+    if (!(resp.status >= 200 && resp.status <= 299)) {
+      throw Error(resp.statusText);
+    }
+  }).then(() => {
+    feedback('Message sent successfully', LEVELS.SUCCESS);
+    form.reset();
+  }).catch(error => {
+    feedback(error, LEVELS.DANGER);
+  });
+}
+
+document
+  .querySelector('form[name=contact]')
+  .addEventListener('submit', handleSubmit);
